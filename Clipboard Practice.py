e42a20a4d04d3493dfac4139c759f9f1010d73c8
sigmaclover <59852284+sigmaclover@users.noreply.github.com>
2020-01-17 07:01:30 -0500

    Add files via upload
    Continuously checks for "bad" words

--- Clipboard Practice.py
+++ Clipboard Practice.py
@@ -1,9 +1,5 @@
 import pyperclip
 
-if "bad" in pyperclip.paste():
-    print("You shouldn't do that")
-    pyperclip.copy("good")
-
-    print(pyperclip.paste())
-else:
-    print("all good")
+while True:
+    if "bad" in pyperclip.paste():
+        pyperclip.copy("good")
